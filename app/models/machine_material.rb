d6f917868b234910fb55475a16307244d0cdb723
ilias hicham <ilias.hicham@gadz.org>
2015-02-27 11:23:42 +0100

    cocoon fixed

--- app/models/machine_material.rb
+++ app/models/machine_material.rb
@@ -1,5 +1,5 @@
 class MachineMaterial < ActiveRecord::Base
-  belongs_to :machine, inverse_of: :machine_materials
+  belongs_to :machine
   belongs_to :material
 
   validates :colors, presence: true
