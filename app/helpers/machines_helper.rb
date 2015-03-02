b23dd5333af76d8aad772a3acad7793b6186fbf7
ilias hicham <ilias.hicham@gadz.org>
2015-03-02 10:48:00 +0100

    icon added to category

--- app/helpers/machines_helper.rb
+++ app/helpers/machines_helper.rb
@@ -1,2 +1,9 @@
 module MachinesHelper
+  def model_picture(version=nil)
+    if machine.machine_model.model_picture
+      machine.machine_model.model_picture
+    else
+      machine.category.icon
+    end
+  end
 end
--- app/models/category.rb
+++ app/models/category.rb
@@ -1,5 +1,11 @@
 class Category < ActiveRecord::Base
   has_many :machine_models, dependent: :destroy
 
+  has_attached_file :icon,
+    styles: { medium: "300x300>", little: "200x200>", thumb: "100x100>" }
+
+  validates_attachment_content_type :icon,
+    content_type: /\Aimage\/.*\z/
+
   validates :name, presence: true
 end
