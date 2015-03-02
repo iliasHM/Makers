c1a6c984338f89e17e1b43277d9ddd62943bef75
ilias hicham <ilias.hicham@gadz.org>
2015-03-02 17:46:16 +0100

    active admin config with pictures

--- app/admin/category.rb
+++ app/admin/category.rb
@@ -1,14 +1,38 @@
 ActiveAdmin.register Category do
 
-  permit_params :name
+  permit_params :name, :icon
 
-  index do
+  index do |ad|
     selectable_column
     column :id
-    column :icon
+    column "Icon" do |event|
+    image_tag(event.icon.url(:little))
+    end
     column :name
     actions
   end
+
+  form :html => {:multipart => true} do |f|
+  f.inputs "Category Details" do
+    f.input :name
+    f.input :icon, required: false, :as => :file
+    # Will preview the image when the object is edited
+  end
+  f.actions
+ end
+
+ show do |ad|
+   attributes_table do
+     row :name
+     row :icon_file_name
+     row :icon do
+       image_tag(ad.icon.url(:medium))
+     end
+     # Will display the image on show object page
+   end
+  end
+
+end
 
   # See permitted parameters documentation:
   # https://github.com/activeadmin/activeadmin/blob/master/docs/2-resource-customization.md#setting-up-strong-parameters
@@ -24,4 +48,3 @@ ActiveAdmin.register Category do
   # end
 
 
-end
